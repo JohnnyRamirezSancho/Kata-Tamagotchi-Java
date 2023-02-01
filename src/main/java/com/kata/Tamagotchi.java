e51e7fc89287acba89392b3124bb5169a6f0da80
JohnnyRamirezSancho <info@johnnyramirez.es>
2023-02-01 09:47:35 +0100

    Test face energy

--- src/main/java/com/kata/Tamagotchi.java
+++ src/main/java/com/kata/Tamagotchi.java
@@ -2,8 +2,104 @@ package com.kata;
 
 public class Tamagotchi {
     
-    int mood = 4;
-    int hunger = 4;
-    int energy = 4;
+    private int mood = 4;
+    private int hunger = 4;
+    private int energy = 4;
+    private String faceActual;
+    private String faceNormal = ":-|";
+    private String faceTired = "(-_-)";
+    private String faceAsleep = "(-_-)zZZ";
+    private String faceHappy = ":-)";
+    private String faceAngry = "ఠ_ఠ";
 
+    public Tamagotchi() {
+        this.faceActual = this.faceNormal;
+    }
+
+    public String getFaceActual() {
+        return faceActual;
+    }
+    public void setFaceActual(String face) {
+        this.faceActual = face;
+    }
+    public String getFaceNormal() {
+        return faceNormal;
+    }
+    public String getFaceTired() {
+        return faceTired;
+    }    
+    public String getFaceAsleep() {
+        return faceAsleep;
+    }    
+    public String getFaceHappy() {
+        return faceHappy;
+    }    
+    public String getFaceAngry() {
+        return faceAngry;
+    }    
+    public int getMood() {
+        return mood;
+    }
+    public void setMood(int mood) {
+        this.mood = mood;
+    }
+    public int getHunger() {
+        return hunger;
+    }
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
+    }
+    public int getEnergy() {
+        return energy;
+    }
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public void modifyFace() {
+        if(this.energy == 0) {
+            this.setFaceActual(this.faceAsleep);
+        }
+        if(this.energy < 3) {
+            this.setFaceActual(this.faceTired);
+        }
+        if(this.mood < 2) {
+            this.setFaceActual(this.faceAngry);
+        }
+        if(this.mood > 8) {
+            this.setFaceActual(this.faceHappy);
+        }
+        if(this.mood <= 8 && this.mood >=2) {
+            this.setFaceActual(this.faceNormal);
+        }
+    }
+
+    public void goSleep() {
+        this.setEnergy(getEnergy() + 2);
+    }
+
+    public void goPlay() {
+        this.setEnergy(getEnergy() - 1);
+        this.setHunger(getHunger() + 1);
+        this.setMood(getMood() + 1);
+    }
+
+    public void goEat() {
+        this.setHunger(getHunger() - 2);
+        this.setEnergy(getEnergy() + 1);
+    }
+
+    public String lifeTamagotchi() {
+        if(getEnergy() < 3) {
+            goSleep();
+        }
+        if(getMood() < 2) {
+            goPlay();
+        }
+        if(getHunger() > 4) {
+            goEat();
+        }
+        modifyFace();
+        return getFaceActual();
+    }
 }
--- src/test/java/com/kata/TamagotchiTest.java
+++ src/test/java/com/kata/TamagotchiTest.java
@@ -1,15 +1,51 @@
 package com.kata;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 public class TamagotchiTest {
     
+    Tamagotchi myLittleTama;
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void new_tamagotchi_have_4_mood()
     {
-        assertTrue( true );
+        myLittleTama = new Tamagotchi();
+        assertEquals(4, myLittleTama.getMood());
+    }
+
+    @Test
+    public void new_tamagotchi_have_4_hunger()
+    {
+        myLittleTama = new Tamagotchi();
+        assertEquals(4, myLittleTama.getHunger());
+    }
+
+    @Test
+    public void new_tamagotchi_have_4_energy()
+    {
+        myLittleTama = new Tamagotchi();
+        assertEquals(4, myLittleTama.getEnergy());
+    }
+
+    @Test
+    public void new_tamagotchi_have_faceActual()
+    {
+        myLittleTama = new Tamagotchi();
+        assertEquals(":-|", myLittleTama.getFaceActual());
+    }
+
+
+
+    @Test
+    public void if_tamagotchi_have_less_3_energy_then_faceTired()
+    {
+        myLittleTama = new Tamagotchi();
+        myLittleTama.setEnergy(2);
+        myLittleTama.modifyFace();
+        assertEquals(2, myLittleTama.getEnergy());
+        assertEquals("(-_-)", myLittleTama.getFaceActual());
     }
 
 }
